696d0f59c630822c7fc08bb34a469b7255dc5294
zhang wei <andyzhang0607@gmail.com>
2019-03-10 18:06:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/*var a = 1
+
+alert(a)
+alert("Checking If You Have Permission To Enter This Web")
+alert("Done")
+if (a==1) {
+	alert("You Have Permissions :)")	
+}
+else {
+	alert("You Don't Have Permissions :(")
+}
+var arrayA = [1, 2, 5, 6, 10]
+
+alert(arrayA);
+arrayA.push("NEW ELEMENT!")
+alert(arrayA) 
+var boo = false && true || !(true && true)
+alert(boo)*/
+
+/*function addSquares(x, y) {
+	return(x*x + y*y)
+}*/
+
+
+//alert(addSquares(2,3))
+
+function concat(str1, str2) {
+	return(str1 + str2);
+
+}
+var newstring = concat("hello ", "world")
+
+//alert(newstring)
+
+function triProduct(a, b, c) {
+	alert(a*b*c)
+}
+alert(triProduct(10, 11, 12))
